9eeae417921bcda9e6a6e018d380473a0334198e
aaadriana <adriana@stanciu.uk>
2021-10-21 12:20:03 +0100

    added post function -pictures page
    Co-authored-by: Elena <76445935+elenamarinaki@users.noreply.github.com>

--- src/handler/pictures.js
+++ src/handler/pictures.js
@@ -6,12 +6,14 @@ const layoutHTML = require('../components/html');
 function get(request, response) {
   const picId = request.params.picId;
 
-  model.getSinglePicture(picId).then((pic) => {
-    const html = /*html*/ `
-        <h1>Who do you think committed this crime? 🤔 </h1>
+  model
+    .getSinglePicture(picId)
+    .then((pic) => {
+      const html = /*html*/ `
+        <h3>Who do you think committed this crime? 🤔 </h3>
         <img src='/picture-temp/${picId}' alt="mystery picture">
         
-        <form action="/pictures/:picid" method="POST">
+        <form action="/pictures/${picId}" method="POST">
             <label for="guess">Make your guess!</label>
             <input type="text" id="guess" name="guess" required>
            <button type="submit">Submit</button>
@@ -19,11 +21,25 @@ function get(request, response) {
         <a href="/">↩ Back to Homepage</a>
         `;
 
-    response.send(layoutHTML('Clue Picture', html)).catch((error) => {
+      response.send(layoutHTML('Clue Picture', html));
+    })
+    .catch((error) => {
       console.error(error);
     });
-  });
 }
 
-function post(request, response) {}
+function post(request, response) {
+  const guess = request.body.guess;
+  const picId = request.params.picId;
+
+  return model
+    .storeGuess(guess, picId)
+    .then(() => response.redirect(`/pictures/${picId}`))
+    .catch((error) => {
+      console.error('error', error);
+      return response.send(
+        `<h1>Unable to post your guess! :(</h1><a href="/">↩ Back to Homepage</a>`
+      );
+    });
+}
 module.exports = { get, post };
